274255ee19d11d6db51f6409c74e536f5307f48f
Alberto Vosgerau <alberto@strawbfields.com>
2021-09-23 02:10:55 -0300

    Initial commit

---
+++ Core/Locators/ControllerLocator.cs
@@ -0,0 +1,7 @@
+namespace UnityMVC.Locator
+{
+    public class ControllerLocator : Locator<Controller.Controller>
+    {
+        
+    }
+}
---
+++ Core/Src/Controller.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using UnityEngine;
+
+namespace UnityMVC.Controller
+{
+    public abstract class Controller
+    {
+        public abstract void SetView(View.View view);
+        public abstract View.View GetView();
+        public abstract bool IsActive();
+        protected void DontDestroyOnLoad(View.View view)
+        {
+            view.transform.parent = null;
+            Object.DontDestroyOnLoad(view);
+        }
+
+        protected abstract void InternalAwake();
+        protected abstract void InternalOnDestroy();
+        protected abstract void InternalOnEnable();
+        protected abstract void InternalOnDisable();
+        
+        protected abstract void SolveDependencies();
+        protected abstract void RegisterEvents();
+        protected abstract void UnregisterEvents();
+
+        public void OnViewAwake()
+        {
+            SolveDependencies();
+            InternalAwake();
+            RegisterEvents();
+            MVCAwake();
+        }
+        public void OnViewStart()
+        {
+            CoroutineHelper.StartCoroutine(this,LateStartRoutine());
+            MVCStart();
+        }
+        
+        public void OnViewUpdate()
+        {
+            MVCUpdate();
+        }
+        
+        public void OnViewOnEnable()
+        {
+            InternalOnEnable();
+            MVCOnEnable();
+        }
+        public void OnViewOnDisable()
+        {
+            InternalOnDisable();
+            MVCOnDisable();
+        }
+        public void OnViewDestroy()
+        {
+            UnregisterEvents();
+            MVCOnDestroy();
+            CoroutineHelper.StoppAllCoroutinesFromSender(this);
+            InternalOnDestroy();
+        }
+        protected IEnumerator LateStartRoutine()
+        {
+            yield return null;
+            MVCLateStart();
+        }
+
+        protected virtual void MVCAwake() {}
+        protected virtual void MVCStart() {}
+        protected virtual void MVCUpdate() {}
+        protected virtual void MVCOnEnable() {}
+        protected virtual void MVCOnDisable() {}
+        protected virtual void MVCOnDestroy() {}
+        protected virtual void MVCLateStart()
+        {
+        }
+    }
+}
